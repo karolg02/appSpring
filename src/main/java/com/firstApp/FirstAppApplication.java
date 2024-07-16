e4318790feb8ff16c429668109fdf915a1da6204
karolg <karolglanowski02@gmail.com>
2024-07-16 19:05:09 +0200

    worksxx

--- src/main/java/com/firstApp/FirstAppApplication.java
+++ src/main/java/com/firstApp/FirstAppApplication.java
@@ -9,6 +9,8 @@ public class FirstAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAppApplication.class, args);
 		System.out.println("XD");
+		//lada
+
 	}
 
 }
